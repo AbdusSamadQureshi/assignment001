551c2b2b5778c4f8f98e5ef401cd0ccb8ef28c20
AbdusSamadQureshi <abdussamadqureshi@yahoo.com>
2020-07-13 16:02:46 +0500

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -2,25 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({name}) {
+  return <div class="myname" >Hello World <strong>{name}</strong>
+  <h1>Heading this is Heading</h1>
+  <ul>
+    <li>firstpoint</li>
+    <li>secondpoint</li>
+  </ul>
+  <ol>
+    <li>1stPoint</li>
+    <li>2ndPoint</li>
+  </ol>
+  {5 + 10}
+   </div>
+ 
 }
 
 export default App;
